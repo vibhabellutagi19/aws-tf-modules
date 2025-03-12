3ef62fbb209aa19ab197580f6feadd385e843917
buildwith_vibs <vibhavari.bellutagi@gmail.com>
2025-03-12 16:26:51 +0100

    fix the tags for s3

--- components/s3/main.tf
+++ components/s3/main.tf
@@ -1,5 +1,5 @@
 # Create S3 Bucket
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "s3_bucket" {
   bucket        = var.bucket_name != null ? var.bucket_name : "${var.project_key}-${var.bucket_prefix}"
   force_destroy = var.force_destroy
 
@@ -10,8 +10,8 @@ resource "aws_s3_bucket" "this" {
 }
 
 # Attach an S3 Bucket Policy (if provided)
-resource "aws_s3_bucket_policy" "this" {
+resource "aws_s3_bucket_policy" "bucket_policy" {
   count  = var.custom_bucket_policy_json != "" ? 1 : 0
-  bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.s3_bucket.id
   policy = var.custom_bucket_policy_json
 }
--- components/s3/output.tf
+++ components/s3/output.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "The name of the created S3 bucket."
-  value       = aws_s3_bucket.this.bucket
+  value       = aws_s3_bucket.s3_bucket.bucket
 }
 
 output "bucket_arn" {
   description = "The ARN of the created S3 bucket."
-  value       = aws_s3_bucket.this.arn
+  value       = aws_s3_bucket.s3_bucket.arn
 }
 
 output "bucket_policy_arn" {
   description = "The ARN of the S3 Bucket Policy (if created)."
-  value       = length(aws_s3_bucket_policy.this) > 0 ? aws_s3_bucket_policy.this[0].id : null
+  value       = length(aws_s3_bucket_policy.bucket_policy) > 0 ? aws_s3_bucket_policy.bucket_policy[0].id : null
 }
