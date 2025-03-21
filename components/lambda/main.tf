e894b7f46d2353cdd2af4febd90face0bc2ac95b
buildwith_vibs <vibhavari.bellutagi@gmail.com>
2025-03-21 16:25:12 +0100

    fix lambda

--- components/lambda/main.tf
+++ components/lambda/main.tf
@@ -5,7 +5,7 @@ data "archive_file" "lambda" {
   output_path = var.lambda_object_path
 }
 
-resource "aws_lambda_function" "lambda_function" {
+resource "aws_lambda_function" "lambda" {
   function_name = "${var.project_key}-${var.lambda_name}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = var.handler
